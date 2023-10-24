85ff1d35ef34aa075a836674df37ac917d9b4b5a
andersonvilla <estiven.villa@udea.edu.co>
2023-10-24 17:45:50 -0500

    Lab2 first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LabsCM20232-Gr06"
+include(":Lab2-UI")
+ 
